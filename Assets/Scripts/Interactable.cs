c346d4b8f66b31922da2e84bd348117ad4d740f1
Andrew Graham <andrew.t.graham@live.com>
2015-08-12 14:35:27 -0700

    Rough level design, basic interaction system

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+	public bool interact = false;	// Object is un-interactable by default
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// On mouse hover make the object interactable
+	public void OnMouseEnter(){
+		this.interact = true;
+		Debug.Log (interact);
+	}
+
+	// Upon mouse exit put it back to un-interactable
+	public void OnMouseExit(){
+		this.interact = false;
+		Debug.Log (interact);
+	}	
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController))] 
+
+// Simple player movement with WASD/Arrow Keys
+public class PlayerController : MonoBehaviour {
+
+	public float baseSpeed = 3;
+	public float rotSpeed = 3;
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		// Forward movement and speed of the player
+		Vector3 forward = transform.TransformDirection (0, 0, 1);
+		float currentSpeed = baseSpeed * moveVertical;  
+
+		// Rotational facing of the player
+		transform.Rotate (0, moveHorizontal * rotSpeed, 0);
+
+		// Let's move forward in the direction we're currently facing
+		controller.SimpleMove (forward * currentSpeed);
+	}
+}
